e9bdf8d35d68f86bd99c035c8ed57261cb96f816
D Hainsch <david.hainsch@gmx.de>
2021-08-17 21:02:49 +0200

    Serial data read function implemented. Save to CSV Functionality in get_data() implemented. _init not yet functioning

--- api.py
+++ api.py
@@ -3,18 +3,39 @@
 #last revision: 2021-Aug-09
 
 
-class device:
-    name = ""
-    serial_type = ""
-    previous = None
-    next = None
 
-    def __init__(self, name, serial_type):
+class device():
+
+    def __init__(self, name, port, baud = 9600, bytesize = 8, timeout = 5, stopbits = 1, parity = 'none', lines = 1):
         self.name = name
-        self.serial_type = serial_type
+        self._port = port
+        self._baudrate = baud
+        self._bytesize = bytesize
+        self._timeout = timeout
+        self._stopbits = stopbits
+        self._parity = parity
+        self._lines = lines
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, port):
+        self._port = port
+
+    @property
+    def baudrate(self):
+        return self._baudrate
+
+    @baudrate.setter
+    def baudrate(self, baud):
+        if baud > 0:
+            self._baudrate = baud
+        else:
+            raise Exception("Error: Baudrate cannot be 0 or lower")
+    
+
+All_Devices = {"DSO138mini" : device("DSO138mini", 'COM5', 115200, lines = 1024+17),}
 
 
-All_Devices = {"DSO138mini" : device("DSO138mini", "8N1"),}
-
-def setcurrent(name):
-    return All_Devices[name]
--- wavereader.py
+++ wavereader.py
@@ -18,55 +18,114 @@
 
 import api
 import serial
+import serial.tools.list_ports as port_list
 import time
 import csv
+#---------------------------------------------------GLOBAL VARIABLES---------------------------------------------------#
 
-##def switch(arg):
-##    case = 
-##    if arg == 
+myPort = serial.Serial()
+min_lines = 0
+startup = False
 
-#def interpreter(str):
-    #if()
+#---------------------------------------------------CLASSES---------------------------------------------------#
+
+
+#---------------------------------------------------FUNCTIONS---------------------------------------------------#
     
-#def set_dev(var0):
-    
-def initt(port, baud = 9600, bytesize = 8, timeout = 5, stopbits = 1, parity = "even"):
+def _init(port, baud = 9600, bytesize = 8, timeout = 5, stopbits = 1, parity = 'none'):
     try:
         if stopbits == 0 or stopbits > 2:
-            raise Exception("Error in function call initt(" + port + str(baud) + str(bytesize) + str(timeout) + str(stopbits) + str(parity) + ")\nArgument stopbits must be either 1 (one) or 2 (two)")
+            raise Exception("Error in function call _init(" + port + str(baud) + str(bytesize) + str(timeout) + str(stopbits) + str(parity) + ")\nArgument stopbits must be either 1 (one) or 2 (two)")
             
         else:
-            ser = serial.Serial(port = port, baudrate = baud, bytesize = bytesize, timeout = timeout, stopbits = serial.STOPBITS_ONE if stopbits == 1 else serial.STOPBITS_TWO)
-            return ser
+            ports = list(port_list.comports())
+            i = 0
+            found = False
+            for p in ports:
+                i+=1    
+                print( str(i) + " >> " + str(p)[0:4])
+
+                if str(p)[0:4] == port:
+                    print("COM Port found! Opening port " + port)
+                    found = True
+                    break
+
+                if found:
+                    #return serial.Serial(port = port, baudrate = baud, bytesize = bytesize, timeout = timeout, stopbits = serial.STOPBITS_ONE if stopbits == 1 else serial.STOPBITS_TWO)
+                    myPort.port = port
+                    myPort.baudrate = baud
+                    myPort.open()
+                    return
+
+                raise Exception("COM Port " + port + " not available. Call show_ports() to see full list of ports")    
+
     except:
-        print("Exiting initt()")
+        print("Exiting _init()")
 
 
-def get_data(open_port,var0=None):
+def close(port):
     try:
-        if open_port.in_waiting > 0:
-            
-            open_port.flushInput()
-            ser_data = open_port.readline()
-            print(ser_data)
+        if myPort.port == port and myPort.is_open == True:
+            myPort.close()
+        else:
+            raise Exception("Target Port is not the current open port")
 
-            #decoded_data = float(ser_data[0:len(ser_data)-2].decode("utf-8"))
+    except:
+        print("Exiting close()")
+
+
+def show_ports():
+    ports = list(port_list.comports())
+    for p in ports:
+        print(p)
+
+
+def get_data():
+        decoded_list = []
+        print("Waiting for data")
+        for i in range(min_lines):
+            myPort_data = myPort.readline()
+            decoded_data = myPort_data.decode("Ascii")
+
+            decoded_list.append(decoded_data)
             #print(decoded_data)
 
-            with open("DSO138_data_" + str(time.time()) + ".csv","a") as f:
-                writer = csv.writer(f,delimiter=", ")
-                writer.writerow([time.time(), decoded_bytes])
+        with open("DSO138_data_" + str(time.time()) + ".csv","a") as target_file:
+            for line in decoded_list:        
+                writer = csv.writer(target_file,delimiter=' ')
+                writer.writerow(line)
 
-    except:
-        print("Exiting get_data()")
         
+#---------------------------------------------------DEVICE LIST---------------------------------------------------#
 
+#All_Devices = {"DSO138mini" : api.device("DSO138mini", 'COM5', 115200),}
+
+#---------------------------------------------------RUNTIME---------------------------------------------------#
+
+if False:
+    myPort = serial.Serial('COM5', 115200)
+    for i in range(1024+17):
+        myPort_data = myPort.readline()
+        print(myPort_data.decode("Ascii"))
+
+Run = True
 while True:
-    myport = initt("COM7", 115200)
+    if startup == False:
+        #_init('COM5', 115200)
+        myPort = serial.Serial('COM5', 115200)
+        startup = True
+    
+    min_lines = 1024+17
+
     try:
-        get_data(myport)
+        command = input( str(myPort.port) + ">>")
+        if command == "get data":
+            print("Ready for data")
+            get_data()
+
+        Run = False
 
     except:
         print("Keyboard interrupt")
         break
-    ##x = raw_input("" + ">>: ")
+    
