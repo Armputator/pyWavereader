940913c03bdc223e50b1972d5e41e2c63c397f93
D Hainsch <david.hainsch@gmx.de>
2021-11-17 16:04:02 +0100

    first draft of man.py -> use re library for searching man page in directory

--- src/man.py
+++ src/man.py
@@ -1,1 +1,12 @@
-## to be filled
+import re
+import json
+import os
+
+def print_man(cmnd_name, output):
+    file = open("find_man()")
+
+def find_man():
+    dir = os.listdir('../man')
+    for d in dir:
+        
+    return 
